ed9ba66eafc0e8beba266c445e151eb074fbff97
unknown <https://github.com/bhuwanchaudhary715/Java-with-Dsa.git>
2024-06-19 03:35:08 +0530

    binarySearch

--- src/Searching_Sorting_and_BitManipulation/Searching_Assignment/_3_problem.java
+++ src/Searching_Sorting_and_BitManipulation/Searching_Assignment/_3_problem.java
@@ -16,17 +16,37 @@ import java.util.Scanner;
   // -------------->  efficiently count the total number of 1’s in it <----------------------
 class efficiencyCount{
 
-    public static int totalNumbersOf_1s(int arr[]){
+    // ------> using simple approach LinearSearch <-------------------
+//    public static int totalNumbersOf_1s(int arr[]){
+//
+//        int count=0;
+//        for(int i=0;i<arr.length;i++){
+//
+//            if(arr[i] == 1){
+//
+//                count++;
+//            }
+//        }
+//        return count;
 
-        int count=0;
-        for(int i=0;i<arr.length;i++){
 
-            if(arr[i] == 1){
+      public static int totalNumbersOf_1s(int arr[]){
 
-                count++;
-            }
-        }
-        return count;
+          int low=0, high=arr.length-1, mid=0;
+
+          while (low <= high){
+
+              mid=low+(high-low)/2;
+
+              if(arr[mid] == 0){
+
+               low=mid+1;
+              }else {
+
+                  high=mid-1;
+              }
+          }
+          return arr.length-low;
     }
 }
 
